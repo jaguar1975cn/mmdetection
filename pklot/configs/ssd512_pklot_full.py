2e85fe5d260e2d7012c39f84ee292e1219dc8852
James Zeng <james.zeng@gmail.com>
2024-03-06 23:22:38 +0000

    add ssd512_pklot_full.py

---
+++ pklot/configs/ssd512_pklot_full.py
@@ -0,0 +1,62 @@
+_base_ = '../../configs/ssd/ssd512_coco.py'
+
+custom_imports = dict(
+    imports=['pklot'],
+    allow_failed_imports=False)
+
+max_epochs = 40
+train_cfg = dict(max_epochs=max_epochs)
+
+model = dict(
+    bbox_head=dict(num_classes=3, type="MySSDHead"))
+
+data_root = './datasets/pklot/images/'
+metainfo = {
+    'classes': ('spaces', 'space-empty', 'space-occupied'),
+    'palette': [
+        (220, 220, 220),
+        (20, 220, 60),
+        (220, 20, 60),
+    ]
+}
+annotation_file = 'full3.json'
+train_dataloader = dict(
+    batch_size=8,
+    dataset=dict(
+        _delete_=True,
+        type={{_base_.dataset_type}},
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='train/' + annotation_file,
+        data_prefix=dict(img='train/'),
+        pipeline=_base_.train_pipeline
+    )
+)
+val_dataloader = dict(
+    dataset=dict(
+        _delete_=True,
+        type={{_base_.dataset_type}},
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='valid/' + 'full3-1024.json',
+        data_prefix=dict(img='valid/'),
+        pipeline=_base_.test_pipeline
+    )
+)
+
+test_dataloader = dict(
+    dataset=dict(
+        _delete_=True,
+        type={{_base_.dataset_type}},
+        metainfo=metainfo,
+        data_root=data_root,
+        ann_file='test/' + annotation_file,
+        data_prefix=dict(img='test/'),
+        pipeline=_base_.test_pipeline
+    )
+)
+
+
+# Modify metric related settings
+val_evaluator = dict(ann_file=data_root + 'valid/' + annotation_file, metric=['bbox'])
+test_evaluator = dict(ann_file=data_root + 'test/' + annotation_file, metric=['bbox'])
