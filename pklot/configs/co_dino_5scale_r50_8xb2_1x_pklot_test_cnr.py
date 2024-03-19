08879939c71cfb090e205fefa0f3416d4537f02b
James Zeng <james.zeng@deepnetsecurity.com>
2024-03-19 23:47:56 +0000

    Add co-detr-cnr-test

---
+++ pklot/configs/co_dino_5scale_r50_8xb2_1x_pklot_test_cnr.py
@@ -0,0 +1,57 @@
+_base_ = '../../projects/CO-DETR/configs/codino/co_dino_5scale_r50_8xb2_1x_coco.py'
+
+auto_scale_lr = dict(base_batch_size=8)
+
+max_epochs = 40
+train_cfg = dict(max_epochs=max_epochs, type='EpochBasedTrainLoop', val_interval=1)
+default_hooks = dict(
+    checkpoint=dict(interval=1, type='CheckpointHook'))
+
+def override_dict_functional(original_dict, key_to_override, new_value):
+    return dict(map(lambda item: (key_to_override, new_value) if item[0] == key_to_override else item, original_dict.items()))
+
+num_classes = 3
+
+bbox_head=override_dict_functional(_base_.model['roi_head'][0]['bbox_head'], 'num_classes', num_classes)
+
+model = dict(
+    bbox_head=[override_dict_functional(_base_.model.bbox_head[0], 'num_classes', num_classes)],
+    query_head=dict(num_classes=num_classes),
+    roi_head=[
+        override_dict_functional(_base_.model.roi_head[0], 'bbox_head', bbox_head)
+    ],
+)
+
+data_root = './datasets/cnr/'
+metainfo = {
+    'classes': ('spaces', 'space-empty', 'space-occupied'),
+    'palette': [
+        (220, 220, 220),
+        (20, 220, 60),
+        (220, 20, 60),
+    ]
+}
+annotation_file = 'cnr-coco.json'
+train_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='train/' + annotation_file,
+        data_prefix=dict(img='train/')))
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='valid/' + annotation_file,
+        data_prefix=dict(img='valid/')))
+test_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file=annotation_file,
+        data_prefix=dict(img='')))
+
+# Modify metric related settings
+# val_evaluator = dict(ann_file=data_root + 'valid/' + annotation_file, metric=['bbox'])
+test_evaluator = dict(ann_file=data_root + annotation_file, metric=['bbox'])
