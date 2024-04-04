70eb3b1b56939e12349b30848a78c3d4f8939b03
James Zeng <james.zeng@deepnetsecurity.com>
2024-04-04 23:14:56 +0100

    Add swin pklot

---
+++ pklot/configs/mask-rcnn_swin-t-p4-w7_fpn_amp-ms-crop-3x_pklot.py
@@ -0,0 +1,54 @@
+_base_ = '../../configs/swin/mask-rcnn_swin-t-p4-w7_fpn_amp-ms-crop-3x_coco.py'
+
+
+auto_scale_lr = dict(base_batch_size=8)
+
+max_epochs = 40
+train_cfg = dict(max_epochs=max_epochs, type='EpochBasedTrainLoop', val_interval=1)
+default_hooks = dict(
+    checkpoint=dict(interval=1, type='CheckpointHook'))
+
+model = dict(
+    roi_head=dict(
+        bbox_head=dict(
+            num_classes=3
+        ),
+        mask_head=dict(
+            num_classes=3
+        )
+    )
+)
+
+data_root = './datasets/pklot/images/'
+metainfo = {
+    'classes': ('spaces', 'space-empty', 'space-occupied'),
+    'palette': [
+        (220, 220, 220),
+        (20, 220, 60),
+        (220, 20, 60),
+    ]
+}
+annotation_file = '_annotations.coco-seg.json'
+train_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='train/' + annotation_file,
+        data_prefix=dict(img='train/')))
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='valid/' + annotation_file,
+        data_prefix=dict(img='valid/')))
+test_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='test/' + annotation_file,
+        data_prefix=dict(img='test/')))
+
+# Modify metric related settings
+val_evaluator = dict(ann_file=data_root + 'valid/' + annotation_file, metric=['bbox'])
+test_evaluator = dict(ann_file=data_root + 'test/' + annotation_file, metric=['bbox'])
