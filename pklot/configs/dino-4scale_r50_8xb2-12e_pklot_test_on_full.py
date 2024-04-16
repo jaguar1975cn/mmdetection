5e13e96b70fd6dbc36c424c4ad52311239fc3132
James Zeng <james.zeng@gmail.com>
2024-04-16 01:27:30 +0100

    add dino test on full

---
+++ pklot/configs/dino-4scale_r50_8xb2-12e_pklot_test_on_full.py
@@ -0,0 +1,42 @@
+_base_ = '../../configs/dino/dino-4scale_r50_8xb2-12e_coco.py'
+
+max_epochs = 300
+train_cfg = dict(max_epochs=max_epochs, type='EpochBasedTrainLoop', val_interval=1)
+
+model = dict(
+    bbox_head=dict(num_classes=3))
+
+data_root = './datasets/pklot/images/'
+metainfo = {
+    'classes': ('spaces', 'space-empty', 'space-occupied'),
+    'palette': [
+        (220, 220, 220),
+        (20, 220, 60),
+        (220, 20, 60),
+    ]
+}
+#annotation_file = 'mini.json'
+annotation_file = '_annotations.coco-seg.json'
+train_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='train/' + annotation_file,
+        data_prefix=dict(img='train/')))
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='valid/' + annotation_file,
+        data_prefix=dict(img='valid/')))
+test_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='test/full3.json',# + annotation_file,
+        data_prefix=dict(img='test/')))
+
+# Modify metric related settings
+val_evaluator = dict(ann_file=data_root + 'valid/' + annotation_file, metric=['bbox'])
+test_evaluator = dict(ann_file=data_root + 'test/full3.json', metric=['bbox'])
