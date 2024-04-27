61bf45d7cda2472543a88e884556b01ef1dfaf0f
James Zeng <james.zeng@deepnetsecurity.com>
2024-04-27 23:59:28 +0100

    update pklot-cnr

--- pklot/configs/grounding_dino_r50_scratch_8xb2_1x_pklot-cnr.py
+++ pklot/configs/grounding_dino_r50_scratch_8xb2_1x_pklot-cnr.py
@@ -4,12 +4,11 @@ _base_ = '../../configs/grounding_dino/grounding_dino_r50_scratch_8xb2_1x_coco.p
 max_epochs = 40
 train_cfg = dict(max_epochs=max_epochs, type='EpochBasedTrainLoop', val_interval=1)
 default_hooks = dict(
-    checkpoint=dict(interval=5, type='CheckpointHook'))
+    checkpoint=dict(interval=1, type='CheckpointHook'))
 
 model = dict(
     bbox_head=dict(num_classes=3))
 
-#data_root = './datasets/pklot/images/'
 data_root = './datasets/cnr/'
 metainfo = {
     'classes': ('spaces', 'space-empty', 'space-occupied'),
@@ -19,28 +18,28 @@ metainfo = {
         (220, 20, 60),
     ]
 }
-#annotation_file = '_annotations.coco-seg.json'
+
 annotation_file = 'cnr-coco.json'
 train_dataloader = dict(
     batch_size=1,
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='train/' + annotation_file,
-        data_prefix=dict(img='train/')))
+        ann_file='cnr-train.json',
+        data_prefix=dict(img='')))
 val_dataloader = dict(
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='valid/' + annotation_file,
-        data_prefix=dict(img='valid/')))
+        ann_file='cnr-validation.json',
+        data_prefix=dict(img='')))
 test_dataloader = dict(
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file= annotation_file,
+        ann_file='cnr-test.json',
         data_prefix=dict(img='')))
 
 # Modify metric related settings
-#val_evaluator = dict(ann_file=data_root + 'valid/' + annotation_file, metric=['bbox'])
-test_evaluator = dict(ann_file=data_root + annotation_file, metric=['bbox'])
+val_evaluator = dict(ann_file=data_root + 'cnr-validation.json/', metric=['bbox'])
+test_evaluator = dict(ann_file=data_root + 'cnr-test.json', metric=['bbox'])
