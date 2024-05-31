0304122310420676998aa72e37e1c6a9bf579986
James Zeng <james.zeng@gmail.com>
2024-05-31 16:12:43 +0100

    Add configs for pklot new split

--- pklot/configs2/deformable-detr_r50_16xb2-50e_pklot_full_new_split.py
+++ pklot/configs2/deformable-detr_r50_16xb2-50e_pklot_full_new_split.py
@@ -2,7 +2,7 @@ _base_ = '../../configs/deformable_detr/deformable-detr_r50_16xb2-50e_coco.py'
 
 auto_scale_lr = dict(base_batch_size=8)
 
-max_epochs = 290
+max_epochs = 40
 train_cfg = dict(max_epochs=max_epochs, type='EpochBasedTrainLoop', val_interval=1)
 default_hooks = dict(
     checkpoint=dict(interval=1, type='CheckpointHook'))
@@ -12,7 +12,7 @@ model = dict(
     test_cfg=dict(max_per_img=300)
     )
 
-data_root = './datasets/'
+data_root = './datasets/pklot/pklot_full_new_split/'
 metainfo = {
     'classes': ('spaces', 'space-empty', 'space-occupied'),
     'palette': [
@@ -21,26 +21,27 @@ metainfo = {
         (220, 20, 60),
     ]
 }
+annotation_file = 'pklot_ext_annotations.json'
 train_dataloader = dict(
     batch_size=1,
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='merged_train.json',
-        data_prefix=dict(img='')))
+        ann_file='train/' + annotation_file,
+        data_prefix=dict(img='train/')))
 val_dataloader = dict(
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='merged_valid_1024.json',
-        data_prefix=dict(img='')))
+        ann_file='valid/' + annotation_file,
+        data_prefix=dict(img='valid/')))
 test_dataloader = dict(
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='merged_test.json',
-        data_prefix=dict(img='')))
+        ann_file='test/' + annotation_file,
+        data_prefix=dict(img='test/')))
 
 # Modify metric related settings
-val_evaluator = dict(ann_file=data_root + 'merged_valid.json', metric=['bbox'])
-test_evaluator = dict(ann_file=data_root + 'merged_test.json', metric=['bbox'])
+val_evaluator = dict(ann_file=data_root + 'valid/' + annotation_file, metric=['bbox'])
+test_evaluator = dict(ann_file=data_root + 'test/' + annotation_file, metric=['bbox'])
