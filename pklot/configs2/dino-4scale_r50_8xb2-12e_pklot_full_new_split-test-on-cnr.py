722552ea5b8015e2a68e9a47738791988c93a980
James Zeng <james.zeng@deepnetsecurity.com>
2024-06-11 21:52:01 +0100

    Add dino pklot full test-on-cnr

---
+++ pklot/configs2/dino-4scale_r50_8xb2-12e_pklot_full_new_split-test-on-cnr.py
@@ -0,0 +1,43 @@
+_base_ = '../../configs/dino/dino-4scale_r50_8xb2-12e_coco.py'
+auto_scale_lr = dict(base_batch_size=8)
+
+max_epochs = 22
+train_cfg = dict(max_epochs=max_epochs, type='EpochBasedTrainLoop', val_interval=1)
+default_hooks = dict(
+    checkpoint=dict(interval=1, type='CheckpointHook'))
+
+model = dict(
+    bbox_head=dict(num_classes=3))
+
+data_root = './datasets/cnr/'
+metainfo = {
+    'classes': ('spaces', 'space-empty', 'space-occupied'),
+    'palette': [
+        (220, 220, 220),
+        (20, 220, 60),
+        (220, 20, 60),
+    ]
+}
+train_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='cnr-train-2.json',
+        data_prefix=dict(img='')))
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='cnr-validation-2.json',
+        data_prefix=dict(img='')))
+test_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='cnr-test-2.json',
+        data_prefix=dict(img='')))
+
+# Modify metric related settings
+val_evaluator = dict(ann_file=data_root + 'cnr-validation-2.json', metric=['bbox'], proposal_nums=[1000, 1000, 1000], use_mp_eval=True)
+test_evaluator = dict(ann_file=data_root + 'cnr-test-2.json', metric=['bbox'], proposal_nums=[1000, 1000, 1000], use_mp_eval=True)
