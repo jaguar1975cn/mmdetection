3eb58dfd6adcd762925740ad3d9b16b16273de19
James Zeng <james.zeng@deepnetsecurity.com>
2024-07-08 17:58:53 +0100

    correct paths

--- pklot/configs3/dino-4scale_r50_8xb2-12e_pklot_ext_no_dup-test-on-cnr-scaled.py
+++ pklot/configs3/dino-4scale_r50_8xb2-12e_pklot_ext_no_dup-test-on-cnr-scaled.py
@@ -9,7 +9,7 @@ default_hooks = dict(
 model = dict(
     bbox_head=dict(num_classes=3))
 
-data_root = './datasets/cnr/scaled'
+data_root = './datasets/cnr/scaled/'
 metainfo = {
     'classes': ('spaces', 'space-empty', 'space-occupied'),
     'palette': [
@@ -18,27 +18,27 @@ metainfo = {
         (220, 20, 60),
     ]
 }
-annotation_file = 'cnr-coco.json'
+annotation_file = 'cnr.json'
 train_dataloader = dict(
     batch_size=1,
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='cnr-train.json',
-        data_prefix=dict(img='')))
+        ann_file='cnr.json',
+        data_prefix=dict(img='train')))
 val_dataloader = dict(
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='cnr-validation.json',
-        data_prefix=dict(img='')))
+        ann_file='cnr.json',
+        data_prefix=dict(img='valid')))
 test_dataloader = dict(
     dataset=dict(
         data_root=data_root,
         metainfo=metainfo,
-        ann_file='cnr-test.json',
-        data_prefix=dict(img='')))
+        ann_file='cnr.json',
+        data_prefix=dict(img='test')))
 
 # Modify metric related settings
-val_evaluator = dict(ann_file=data_root + 'cnr-validation.json', metric=['bbox'], proposal_nums=[1000, 1000, 1000], use_mp_eval=True)
-test_evaluator = dict(ann_file=data_root + 'cnr-test.json', metric=['bbox'], proposal_nums=[1000, 1000, 1000], use_mp_eval=True)
+val_evaluator = dict(ann_file=data_root + 'valid/' + annotation_file, metric=['bbox'], proposal_nums=[1000, 1000, 1000], use_mp_eval=True)
+test_evaluator = dict(ann_file=data_root + 'test/' + annotation_file, metric=['bbox'], proposal_nums=[1000, 1000, 1000], use_mp_eval=True)
